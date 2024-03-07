0a62237e4aec1b40e3c1ac179e4e3d6e63f0c399
AlenaMau <alenamau3486@gmail.com>
2024-03-07 14:44:40 +0700

    изменение в helpers

--- src/helpers.php
+++ src/helpers.php
@@ -5,3 +5,58 @@ function collection(array $array = []): Collect
 {
    return new Collect($array);
 }
+
+function get(Collect $collection, $key = null)
+{
+    return $collection->get($key);
+}
+
+function first(Collect $collection)
+{
+    return $collection->first();
+}
+
+function count(Collect $collection): int
+{
+    return $collection->count();
+}
+
+function toArray(Collect $collection): array
+{
+    return $collection->toArray();
+}
+
+function map(Collect $collection, callable $callback): Collect
+{
+    return $collection->map($callback);
+}
+
+function each(Collect $collection, callable $callback, ...$args): Collect
+{
+    return $collection->each($callback, ...$args);
+}
+
+function push(Collect $collection, $value, $key = null): Collect
+{
+    return $collection->push($value, $key);
+}
+
+function unshift(Collect $collection, $value): Collect
+{
+    return $collection->unshift($value);
+}
+
+function shift(Collect $collection): Collect
+{
+    return $collection->shift();
+}
+
+function pop(Collect $collection): Collect
+{
+    return $collection->pop();
+}
+
+function splice(Collect $collection, $idx, $length = 1): Collect
+{
+    return $collection->splice($idx, $length);
+}
